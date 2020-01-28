3ac71074803791b6390c2580b409e2805315b295
PoulainMaxime <poulai_a@etna-alternance.net>
2020-01-28 16:07:24 +0100

    install only productoin dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,9 @@
 
 
 if [ -z $2 ]; then
-  $1 install
+  $1 install --production
   $1 run build
-  rm -rf node_modules
 fi
 
-$1 --prefix $2/ install
+$1 --prefix $2/ install --production
 $1 --prefix $2/ run build
-rm -rf $2/node_modules
