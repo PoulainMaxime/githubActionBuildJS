58fff56424f99664c18d53a1c5e64a718e872ddc
PoulainMaxime <poulai_a@etna-alternance.net>
2020-01-28 14:38:16 +0100

    modify entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh -l
 
-cd $2 && $1 install && $1 run build && rm -rf node_modules
+
+if [ -z $2 ]; then
+  $1 install
+  $1 run build
+  rm -rf node_modules
+fi
+
+$1 --prefix $2 install
+$1 --prefix $2 run build
+rm -rf $2/node_modules
