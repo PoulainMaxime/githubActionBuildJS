d94b248e23b708155683104e031b43c203520569
PoulainMaxime <poulai_a@etna-alternance.net>
2020-01-30 09:10:42 +0100

    remove s from production args

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,13 @@ if [ -z $2 ]; then
     $1 install
     $1 run build
     rm -rf $2/node_modules
-    $1 install --productions
+    $1 install --production
   else
     echo "Using YARN in $2 to install dependencies"
     $1 install
     $1 run build
     rm -rf $2/node_modules
-    $1 install --productions
+    $1 install --production
   fi
 fi
 
@@ -23,11 +23,11 @@ if [ "$1" == "npm" ]; then
   $1 --prefix $2/ install
   $1 --prefix $2/ run build
   rm -rf $2/node_modules
-  $1 install --productions
+  $1 install --production
 else
   echo "Using YARN to install dependencies"
   $1 --cwd $2/ install
   $1 --cwd $2/ run build
   rm -rf $2/node_modules
-  $1 install --productions
+  $1 install --production
 fi
