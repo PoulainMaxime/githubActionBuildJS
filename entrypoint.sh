3b4046015a8c006b954e55888c739f7178145f17
PoulainMaxime <poulai_a@etna-alternance.net>
2020-01-30 15:24:47 +0100

    remove code duplicate

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-
 if [ ! -z "$2" ]
 then
   echo "Changing dir to $2"
   cd $2
 fi
 
-echo "Using NPM to install dependencies"
+echo "Using $1 to install dependencies"
 $1 install
 $1 un build
 rm -rf node_modules
