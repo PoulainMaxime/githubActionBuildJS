2c5a045523051d38f0e611f93f2bbcd9dbe5b99c
PoulainMaxime <poulai_a@etna-alternance.net>
2020-01-30 14:53:07 +0100

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,32 +2,22 @@
 set -e
 
 
-if [ -z $2 ]; then
-  if [ "$1" == "npm" ]; then
-    echo "Using NPM in $2 to install dependencies"
-    $1 install
-    $1 run build
-    rm -rf $2/node_modules
-    $1 install --production
-  else
-    echo "Using YARN in $2 to install dependencies"
-    $1 install
-    $1 run build
-    rm -rf $2/node_modules
-    $1 install --production
-  fi
+if [ ! -z "$2" ]
+then
+  echo "Changing dir to $2"
+  cd $2
 fi
 
 if [ "$1" == "npm" ]; then
   echo "Using NPM to install dependencies"
-  $1 --prefix $2/ install
-  $1 --prefix $2/ run build
-  rm -rf $2/node_modules
+  $1 install
+  $1  un build
+  rm -rf node_modules
   $1 install --production
 else
   echo "Using YARN to install dependencies"
-  $1 --cwd $2/ install
-  $1 --cwd $2/ run build
-  rm -rf $2/node_modules
+  $1 install
+  $1 run build
+  rm -rf node_modules
   $1 install --production
 fi
